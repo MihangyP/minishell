fbbf93f54be5bac523febb868a8749b9e1d5e047
MihangyP <pierrotmihangy@gmail.com>
2024-09-19 07:18:55 +0300

    cotinue to implement ast

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -144,6 +144,20 @@ char	**insert_argv(t_token *last)
 	return (argv);
 }
 
+size_t	count_nb_operators(t_token *last)
+{
+	size_t	counter;
+
+	counter = 0;
+	while (last)
+	{
+		if (is_operator(last->text[0]))
+			++counter;
+		last = last->prev;
+	}
+	return (counter);
+}
+
 // TODO
 //t_ast *create_cmd(t_token *t)
 //{
@@ -152,9 +166,12 @@ char	**insert_argv(t_token *last)
 t_ast	*parse(t_token *token_root)
 {
 	t_ast	*ast;
+	t_ast	**tmp;
 	t_token	*last;
+	size_t	nb_operators;
 	
 	ast = NULL;
+	nb_operators = 0;
 	if (!has_operator(token_root))
 	{
 		ast = create_cmd(token_root);
@@ -166,14 +183,33 @@ t_ast	*parse(t_token *token_root)
 		last = tokens_find_last(token_root);	
 		if (!has_pipe(last))
 		{
+			// echo "Donto" > donto.txt >> crimy.txt
 			while (!is_operator(last->text[0]))
 				last = last->prev;
-			ast = new_node(last);
-			if (ast == NULL)
+			tmp = &ast;
+			*tmp = new_node(last);
+			if (*tmp == NULL)
 				return (NULL);
-			ast->right = new_node(last->next);
-			if (ast->right == NULL)
+			(*tmp)->right = new_node(last->next);
+			if ((*tmp)->right == NULL)
 				return (NULL);
+			nb_operators = count_nb_operators(last);
+			while (nb_operators != 0)
+			{
+				last = find_next_ast_node(last->prev);
+				(*tmp)->left = new_node(last);
+				if ((*tmp)->left->identifier == CMD && last->next->identifier == ARGUMENT)
+				{
+					(*tmp)->left->argv = insert_argv(last->next);
+					if ((*tmp)->left->argv == NULL)
+						return (NULL);
+				}
+				else if (is_operator(last->text[0]) && !is_operator(last->next->text[0]))
+					(*tmp)->left->right = new_node(last->next); 		
+				tmp = &(ast->left);
+				--nb_operators;
+			}
+			/*
 			last = find_next_ast_node(last->prev);
 			ast->left = new_node(last);
 			if (ast->left->identifier == CMD && last->next->identifier == ARGUMENT)
@@ -182,6 +218,7 @@ t_ast	*parse(t_token *token_root)
 				if (ast->left->argv == NULL)
 					return (NULL);
 			}
+			*/
 		}
 		/*else*/
 		/*{*/
