feb2493ced86da47b8c5ad97d500ff813be44898
Pierrot Mihangy <pmihangy@c1r7s14.42antananarivo.mg>
2024-10-11 16:13:07 +0300

    try to implement export

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: pmihangy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 10:57:28 by pmihangy          #+#    #+#             */
-/*   Updated: 2024/10/09 13:17:23 by pmihangy         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:11:44 by pmihangy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,14 +41,29 @@ bool	has_equal(char *str)
  *
  */
 
-void	verify_if_valide(char *str)
+size_t	calc_size_str_trimed(char *str)
 {
-	if (str[0] == '\"' || str[0] == '\'')
+	size_t	counter;
+
+	counter = 0;
+
+}
+
+void	verify_if_valid(char *str)
+{
+	char	*str_trimed;
+	size_t	size;
+
+	if (str[i] == '\'' || str[i] == '\"')
+	{
+		size = calc_size_str_trimed(str);
+	}
 }
 
 bool	export_minishell(char **env, char **to_export)
 {
 	int	i;
+	char	*s_trimed;
 
 	if (to_export == NULL)
 	{
@@ -58,16 +73,13 @@ bool	export_minishell(char **env, char **to_export)
 	i = 0;
 	while (to_export[i])
 	{
-		if (has_equal(to_export[i]))
+		// TODO: manage errors
+		if (has_open_quote(to_export[i], false, 0))
 		{
-			printf("%s\n", to_export[i]);
-		}
-		else if (has_open_quote(to_export[i], false, 0))
 			printf("open quote\n");
-		if (!has_equal(to_export[i]))
-		{
-			verify_if_valid(to_export[i]);
+			return (true);
 		}
+		if (has_equal())
 		++i;
 	}
 	return (true);
