417cdeef5a07a193132228418ed5b063a0a2f8d4
Pierrot Mihangy <pmihangy@c1r5s5.42antananarivo.mg>
2024-10-15 14:41:30 +0300

    add manual in export.c file

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,12 +6,109 @@
 /*   By: pmihangy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 10:57:28 by pmihangy          #+#    #+#             */
-/*   Updated: 2024/10/11 16:11:44 by pmihangy         ###   ########.fr       */
+/*   Updated: 2024/10/15 14:11:20 by pmihangy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+
+/*
+ 		MANUEL
+NAME
+    export - Set export attribute for shell variables.
+
+SYNOPSIS
+    export [-fn] [name[=value] ...] or export -p
+
+DESCRIPTION
+    Set export attribute for shell variables.
+    
+    Marks each NAME for automatic export to the environment of subsequently
+    executed commands.  If VALUE is supplied, assign VALUE before exporting.
+    
+    Options:
+      -f	refer to shell functions
+      -n	remove the export property from each NAME
+      -p	display a list of all exported variables and functions
+    
+    An argument of `--' disables further option processing.
+    
+    Exit Status:
+    Returns success unless an invalid option is given or NAME is invalid.
+
+SEE ALSO
+    bash(1)
+
+IMPLEMENTATION
+    GNU bash, version 5.0.17(1)-release (x86_64-redhat-linux-gnu)
+    Copyright (C) 2019 Free Software Foundation, Inc.
+    License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+
+*/
+
+/*
+La commande `export` dans les systèmes UNIX est utilisée pour ajouter ou modifier des variables d'environnement. Ces variables d'environnement sont ensuite accessibles à tous les processus fils créés par le shell.
+
+Voici un aperçu détaillé de la gestion des cas que tu devras prendre en compte pour implémenter cette commande en C, avec des exemples spécifiques comme ceux que tu as fournis :
+
+1. **`export DONTO`**  
+   - **Action** : Ici, on essaie d'exporter la variable `DONTO`, mais elle n’a pas de valeur assignée. Le comportement typique dans le shell est de simplement ajouter `DONTO` aux variables d'environnement sans valeur.
+   - **À gérer** : Ajouter `DONTO` à l'environnement si elle n'existe pas déjà, mais sans valeur associée.
+
+2. **`export "'DONTO'"`**  
+   - **Action** : Ici, le nom de la variable est invalide à cause des guillemets simples autour de `DONTO`.
+   - **À gérer** : Afficher un message d'erreur. Le nom de la variable d'environnement doit être une chaîne sans caractères spéciaux comme des guillemets.
+
+3. **`export "'DONTO'"=DONTO`**  
+   - **Action** : Le nom de la variable est entouré de guillemets simples, ce qui est également invalide.
+   - **À gérer** : Afficher un message d'erreur similaire à celui ci-dessus, car le nom de la variable contient des guillemets.
+
+4. **`export DONTO=DONTO`**  
+   - **Action** : Ici, on assigne la valeur `DONTO` à la variable `DONTO`. Le comportement attendu est d'ajouter `DONTO` à l'environnement avec la valeur `DONTO`.
+   - **À gérer** : Insérer `DONTO=DONTO` dans les variables d'environnement.
+
+5. **`export "DONTO"=DONTO`**  
+   - **Action** : Même si le nom `DONTO` est entouré de guillemets doubles, cela reste valide, car les guillemets sont utilisés pour protéger les espaces ou caractères spéciaux. Le nom de la variable ici est toujours `DONTO`.
+   - **À gérer** : Ajouter `DONTO=DONTO` dans l'environnement.
+
+6. **`export "DONTO=DONTO`**  
+   - **Action** : Il y a une erreur de syntaxe ici, car il manque la fermeture du guillemet. Cela devrait être considéré comme une erreur.
+   - **À gérer** : Afficher un message d'erreur de syntaxe.
+
+7. **`export DONTO="DONTO"`**  
+   - **Action** : Ici, on assigne la valeur `DONTO` à la variable `DONTO`, mais la valeur est entourée de guillemets doubles, ce qui est valide.
+   - **À gérer** : Ajouter `DONTO=DONTO` dans l'environnement.
+
+8. **`export DONTO="'DONTO'"`**  
+   - **Action** : Dans ce cas, on assigne la valeur `'DONTO'` (avec les guillemets simples) à la variable `DONTO`. C'est valide.
+   - **À gérer** : Ajouter `DONTO='DONTO'` dans l'environnement.
+
+### Cas supplémentaires à gérer :
+
+- **`export` sans argument** : Affiche la liste des variables d'environnement.
+- **`export var+=value`** : Certains shells supportent l'addition à la valeur existante d'une variable (non standard, mais utile à considérer).
+- **Noms de variables invalides** : Les noms de variables ne peuvent pas commencer par un chiffre, contenir des caractères spéciaux, etc. Gérer cela avec un message d'erreur.
+
+### Implémentation en C
+
+Ton programme devra :
+1. Vérifier la validité des noms de variables (alphanumériques, pas de caractères spéciaux).
+2. Gérer les erreurs de syntaxe (comme les guillemets non fermés).
+3. Mettre à jour l'environnement en utilisant une fonction comme `setenv()` ou `putenv()` pour ajouter ou modifier les variables.
+
+Cela t'aidera à couvrir les différents cas comme demandé.
+*/
+
+/*
+ * STEPS:
+ * 	1 - manage errors
+ * 	2 - if the argument is valid:
+ * 		- if the variable already exist, we update his value
+ * 		- else we create a new variable at the end
+ 
+ *
+ */
 
 bool	has_equal(char *str)
 {
@@ -27,59 +124,78 @@ bool	has_equal(char *str)
 	return (false);
 }
 
-/*
- * TODO: cases to manage
- *
- * export DONTO => do nothing
- * export "'DONTO'" => show mess error
- * export "'DONTO'"=DONTO show mess error
- * export DONTO=DONTO => insert it in env
- * export "DONTO"=DONTO => insert DONTO=DONTO in env
- * export "DONTO=DONTO => show error mess
- * export DONTO="DONTO" => insert DONTO=DONTO in env
- * export DONTO="'DONTO'" => insert DONTO='DONTO' in env
- *
- */
-
-size_t	calc_size_str_trimed(char *str)
+bool	has_quote(char *str)
 {
-	size_t	counter;
+	int	i;
 
-	counter = 0;
-
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\'' || str[i] == '\"')
+			return (true);
+		++i;
+	}
+	return (false);
 }
 
-void	verify_if_valid(char *str)
+char	*normalize_str(char *str)
 {
-	char	*str_trimed;
+	char	*res;
+	char	quote;
 	size_t	size;
+	int		i;
+	int		j;
 
-	if (str[i] == '\'' || str[i] == '\"')
+	size = ft_strlen(str) - 2;
+	res = malloc((size + 1) * sizeof(char));
+	if (res == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (str[i])
 	{
-		size = calc_size_str_trimed(str);
+		if (str[i] == '\"' || str[i] == '\'')
+		{
+			quote = str[i];
+			++i;
+		}
+		++i;
 	}
 }
 
 bool	export_minishell(char **env, char **to_export)
 {
-	int	i;
-	char	*s_trimed;
+	int		i;
+	char	*str;
 
-	if (to_export == NULL)
+	if (!to_export)
 	{
 		env_minishell(env);
 		return (true);
 	}
+	// TODO:  manage errors
 	i = 0;
 	while (to_export[i])
 	{
-		// TODO: manage errors
 		if (has_open_quote(to_export[i], false, 0))
-		{
 			printf("open quote\n");
-			return (true);
+		if (!has_equal(to_export[i]))
+		{
+			if (has_quote(to_export[i]))
+			{
+				str = normalize_str(to_export[i]);	
+				if (str == NULL)
+					return (false);
+				if (!validate(str))
+				{
+					printf("%s is not valid\n", str);
+				}
+			}
 		}
-		if (has_equal())
+		else
+		{
+			// TODO: do the same thing	
+		}
 		++i;
 	}
 	return (true);
