88f081cf84ce123d7dcfeb778f173eee516d3c83
MihangyP <pierrotmihangy@gmail.com>
2024-11-11 15:19:45 +0300

    implement expander

--- src/main.c
+++ src/main.c
@@ -38,24 +38,31 @@ bool	is_operator(char c)
 	return (c == '<' || c == '>' || c == '|');
 }
 
+bool	is_quote(char c)
+{
+	return (c == '\'' || c == '\"');
+}
+
 int	len_text(char *str)
 {
 	int	len;
 
 	len = 0;
-	while (str[len] && !is_operator(str[len]) && !is_space(str[len]))
+	while (str[len] && !is_operator(str[len]) && !is_space(str[len]) && !is_quote(str[len]))
 		++len;
 	return (len);
 }
 
 //$HOME /
 //echo $HOME "hello $USER" 'ddfjf $PATH'
+// TODO: verify if the text is in env
 //^
 int	line_expanded_len(char *str)
 {
 	int	len;
 	int	i;
 	char	quote;
+	char	*text;
 
 	len = 0;
 	i = 0;
@@ -64,26 +71,26 @@ int	line_expanded_len(char *str)
 		if (str[i] == '\"' || str[i] == '\'')
 		{
 			quote = str[i];
-			while (++i != quote)
+			++i;
+			while (str[i] != quote)
 			{
-				printf("%c", str[i]);
-				/*if (quote == '\'' || str[i] != '$')*/
-				/*{*/
-					/*++len;*/
-					/*++i;*/
-				/*}*/
-				/*else*/
-				/*{*/
-					/*char *text = ft_substr(str, i + 1, len_text(str + i + 1));*/
-					/*if (!text)*/
-						/*error("Cannot substract the string\n");*/
-					/*text = getenv(text);*/
-					/*printf("text: %s\n", text);*/
-					/*len += ft_strlen(text);*/
-					/*i += len_text(str + i);*/
-				/*}*/
+				if (quote == '\'' || str[i] != '$')
+				{
+					++len;
+					++i;
+				}
+				else
+				{
+					text = ft_substr(str, i + 1, len_text(str + i + 1));
+					if (!text)
+						error("Cannot substract the string\n");
+					text = getenv(text);
+					len += ft_strlen(text);
+					i += len_text(str + i);
+				}
 			}
-			/*len += 2;*/
+			len += 2;
+			++i;
 		}
 		else
 		{
@@ -94,11 +101,10 @@ int	line_expanded_len(char *str)
 			}
 			else
 			{
-				char *text = ft_substr(str, i + 1, len_text(str + i + 1));
+				text = ft_substr(str, i + 1, len_text(str + i + 1));
 				if (!text)
 					error("Cannot substract the string\n");
 				text = getenv(text);
-				printf("text: %s\n", text);
 				len += ft_strlen(text);
 				i += len_text(str + i);
 			}
@@ -111,29 +117,79 @@ void	expand(char **line)
 {
 	char	*str;
 	char	*tmp;
+	char	*text;
 	int		i;
 	int		j;
 	int		str_size;
+	char	quote;
 
 	tmp = *line;
 	str_size = line_expanded_len(tmp);
-	printf("%d\n", str_size);
-	/*i  = -1;*/
-	/*j = 0;*/
-	/*while (tmp[++i])*/
-	/*{*/
-		/*if (tmp[i] != '$')*/
-		/*{*/
-			/*str[j] = tmp[i];*/
-		/*}*/
-	/*}*/
-	/**line = str;*/
+	str = malloc((str_size + 1) * sizeof(char));
+	if (!str)
+		error("malloc error");
+	i = 0;
+	j = 0;
+	while (tmp[i])
+	{
+		if (tmp[i] == '\"' || tmp[i] == '\'')
+		{
+			quote = tmp[i];
+			++i;
+			str[j] = quote;
+			++j;
+			while (tmp[i] != quote)
+			{
+				if (quote == '\'' || tmp[i] != '$')
+				{
+					str[j] = tmp[i];
+					++j;
+					++i;
+				}
+				else
+				{
+					text = ft_substr(tmp, i + 1, len_text(tmp + i + 1));
+					if (!text)
+						error("Cannot substract the string\n");
+					text = getenv(text);
+					ft_memcpy(str + j, text, ft_strlen(text));
+					j += ft_strlen(text);
+					i += len_text(tmp + i);
+				}
+			}
+			str[j] = quote;
+			++j;
+			++i;
+		}
+		else
+		{
+			if (tmp[i] != '$')
+			{
+				str[j] = tmp[i];
+				++j;
+				++i;
+			}
+			else
+			{
+				text = ft_substr(tmp, i + 1, len_text(tmp + i + 1));
+				if (!text)
+					error("Cannot substract the string\n");
+				text = getenv(text);
+				ft_memcpy(str + j, text, ft_strlen(text));
+				j += ft_strlen(text);
+				i += len_text(tmp + i);
+			}
+		}
+	}
+	str[j] = '\0';
+	*line = str;
 }
 
 /*bool	lexer(t_token **token_root, char *line, char **env)*/
 bool	lexer(char *line, char **env)
 {
 	expand(&line);
+	printf("line: %s\n", line);
 	/*tokenize(token_root, line);*/
 	// expander && lexer
 	/*if (!replace_dollar(&line, data) || !create_list_token(&data->token, line))*/
